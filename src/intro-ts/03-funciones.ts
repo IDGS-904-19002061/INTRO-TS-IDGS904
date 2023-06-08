1148a17f96a4907dc47011e91d93cda35f2bccec
EdgarPureco <pureco.edgar.1am@gmail.com>
2023-06-07 20:11:35 -0600

    Initial commit

---
+++ src/intro-ts/03-funciones.ts
@@ -0,0 +1,18 @@
+function sumar(a,b):void {
+    console.log(a+b);
+    
+};
+
+const resultado = sumar("Roberto","Cardiel")
+
+function sumar2(a:number,b:number):number {
+    return a+b;
+    
+};
+
+console.log(sumar2(2,3));
+
+function multiplicar(n1:number, n2:number, n3:number):number {
+    var tmp = n1*n2*n3;
+    return tmp;
+}
